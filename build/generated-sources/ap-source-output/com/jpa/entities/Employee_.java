a75ef1c02e6727cb4fee813f4794a35c9bc5f43f
Sampath Thennakoon <sampath.thennakoon@gmail.com>
2015-04-24 11:02:07 +0800

    Add other configurations

---
+++ build/generated-sources/ap-source-output/com/jpa/entities/Employee_.java
@@ -0,0 +1,16 @@
+package com.jpa.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-20T10:02:46")
+@StaticMetamodel(Employee.class)
+public class Employee_ { 
+
+    public static volatile SingularAttribute<Employee, String> firstName;
+    public static volatile SingularAttribute<Employee, String> lastName;
+    public static volatile SingularAttribute<Employee, Integer> employeeId;
+    public static volatile SingularAttribute<Employee, Double> salary;
+
+}
